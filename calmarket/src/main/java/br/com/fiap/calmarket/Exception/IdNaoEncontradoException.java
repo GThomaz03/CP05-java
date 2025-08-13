d44dfede9b9f88984afea1af3d6b07c2c2b6a62f
Francesco Monteiro Di Benedetto <161745078+FrancescoDiBenedetto@users.noreply.github.com>
2025-08-13 20:21:53 -0300

    atualizar o retorno para ids que não foram encontrados

--- calmarket/src/main/java/br/com/fiap/calmarket/Exception/IdNaoEncontradoException.java
+++ calmarket/src/main/java/br/com/fiap/calmarket/Exception/IdNaoEncontradoException.java
@@ -1,5 +1,9 @@
 package br.com.fiap.calmarket.Exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class IdNaoEncontradoException extends RuntimeException {
     public IdNaoEncontradoException(String message) {
         super(message);
