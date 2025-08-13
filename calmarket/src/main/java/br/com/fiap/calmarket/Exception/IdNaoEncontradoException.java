13ff88139b6765306277ff6b08e8b125e89a1990
Francesco Monteiro Di Benedetto <161745078+FrancescoDiBenedetto@users.noreply.github.com>
2025-08-13 20:54:08 -0300

    atualizar a mensagem de retorno ao ter erro de não encontrar o ID

--- calmarket/src/main/java/br/com/fiap/calmarket/Exception/IdNaoEncontradoException.java
+++ calmarket/src/main/java/br/com/fiap/calmarket/Exception/IdNaoEncontradoException.java
@@ -3,7 +3,7 @@ package br.com.fiap.calmarket.Exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "ID NÃO ENCONTRADO")
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class IdNaoEncontradoException extends RuntimeException {
     public IdNaoEncontradoException(String message) {
         super(message);
